4258ed73e8c4f10eef04a72b230a65d111f37cc2
MariaPawlowska <90530451+MariaPawlowska@users.noreply.github.com>
2023-04-18 16:01:02 +0200

    Add files via upload

--- lesson-05/main.cpp
+++ lesson-05/main.cpp
@@ -1,64 +1,90 @@
 #include <iostream>
+#include <map>
 #include<string>
 using namespace std;
-class Animal{
-
+class Cantor {
 public:
-   virtual void Pat(){};
-    virtual void Introduce(){}
+    Cantor() = default;
+    virtual ~Cantor() = default;
 
+    virtual double Rate(const string& abbreviation) const = 0;
 };
 
-class Dog:public Animal{
+/*class Euro{
+public:
+    Euro();
+    string abb;
+    double am;
+    Euro(string abbreviation,double amount): abb(abbreviation), am(amount) {};
+};*/
+
+class FakeUsdCantor : public Cantor {
+public:
+    FakeUsdCantor() {
+        // rates_ map with values for ten most popular currencies relative to USD
+        rates_ = {
+                {"EUR", 0.85},
+                {"JPY", 110.66},
+                {"GBP", 0.73},
+                {"CHF", 0.91},
+                {"CAD", 1.25},
+                {"AUD", 1.34},
+                {"CNY", 6.47},
+                {"HKD", 7.78},
+                {"NZD", 1.44},
+                {"KRW", 1153.89}
+        };
+    }
+
+    double Rate(const std::string& abbreviation) const override {
+        auto it = rates_.find(abbreviation);
+        if (it != rates_.end()) {
+            return it->second;
+        }
+        // Handle error case when currency abbreviation is not found
+        return 0.0;
+    }
+
 private:
-    string name;
-    int age;
-public:
-
-    void Pat() override{
-        cout<<name<<" wags his tail"<<endl;
-    }
-    void Ball() {
-        cout<<name<<" ran for the ball"<<endl;
-    }
-    Dog(int n, string x){
-        this -> name = x;
-        this -> age = n;
-    };
-    void Introduce() override{
-        cout<<"The dog name is: "<<this->name<<endl;
-    }
-
-
+    std::map<std::string, double> rates_;
 };
 
-class Cat:public Animal{
+class Currency {
+public:
+    Currency() = default;
+    virtual ~Currency() = default;
+
+    virtual double ConvertedToDollars(const Cantor& cantor) const = 0;
+    virtual std::string Abbreviation() const = 0;
+    virtual double Amount() const = 0;
+};
+class Euro : public Currency {
 private:
-    string name;
-    int age;
+    string abbrev;
+    double amount_;
+
 public:
-    void Pat() override{
-        cout<<name<<" does purrrrrs"<<endl;
+    Euro(std::string abbreviation, double amount) : abbrev(abbreviation), amount_(amount) {}
+
+    double ConvertedToDollars(const Cantor& cantor) const override {
+        double exchange_rate = cantor.Rate(abbrev);
+        return amount_ / exchange_rate;
     }
-    void Play(){
-        cout<<name<<" play the mouse "<<endl;
+
+    string Abbreviation() const override {
+        return abbrev;
     }
-    Cat(int n, string x){
-        this -> name = x;
-        this -> age = n;
-    }
-    void Introduce() override{
-        cout<<"The cat age is: "<<this->age<<endl;
+
+    double Amount() const override {
+        return amount_;
     }
 };
+
 int main() {
-    Dog dog (5,"czarek");
-    Cat cat (10,"mruczek");
-    dog.Introduce();
-    dog.Pat();
-    dog.Ball();
-    cat.Pat();
-    cat.Play();
-    cat.Introduce();
+    FakeUsdCantor usd;
+    Euro my_currency("JPY", 1000);
+    cout << "Abbreviation: " << my_currency.Abbreviation() << endl;
+    cout << "Amount: " << my_currency.Amount() << endl;
+    cout << "Amount in USD: " << my_currency.ConvertedToDollars(usd) << endl;
     return 0;
 }
